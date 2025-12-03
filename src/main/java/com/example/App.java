592da32ea3467ae99b9ab1f26061f10f9258bf08
DV-Prof-Program <dtvician@icloud.com>
2025-12-03 16:51:58 -0500

    Final Test 2

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -37,7 +37,7 @@ public class App {
         Logger.getLogger("").setLevel(Level.INFO);
         logger.info("Application startup");
 
-        int n = 25;
+        int n = 10;
         logger.info("Computing Fibonacci for n=" + n);
 
         int nthValue = fib(n);
